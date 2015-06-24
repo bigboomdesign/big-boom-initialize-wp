1e60082e9c0bcaa3426a06e7e5edf6bcd6154327
Michael Hull <michael@ashevillemathtutoring.com>
2015-06-24 12:00:23 -0400

    updated version

--- bbd-initialize.php
+++ bbd-initialize.php
@@ -4,7 +4,7 @@ Plugin Name: Big Boom Initialize WP
 Description: Initialize options and sample content for your WordPress site
 Author: Big Boom Design
 Author URI: http://www.bigboomdesign.com
-Version: 0.6.0
+Version: 0.7.0
 */
 
 /* 
